c790642f6c356763ae3c7f76cbb7278b4f31d25b
Casey Schmitz <caseyschmitz0@gmail.com>
2020-12-16 00:32:31 -0600

    class refactoring

--- api/views.py
+++ api/views.py
@@ -3,8 +3,8 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
 from django.contrib.auth.models import User
-from .models import SpeedTest, TestNode
-from .serializers import TestNodeSerializer, SpeedTestSerializer, UserSerializer
+from .models import SpeedTest, SpeedTestClient
+from .serializers import SpeedTestClientSerializer, SpeedTestSerializer, UserSerializer
 from .permissions import IsOwnerOrReadOnly
 
 
@@ -12,7 +12,7 @@ from .permissions import IsOwnerOrReadOnly
 def api_root(request, format=None):
     return Response({
         'speedtest': reverse('speedtest-list', request=request, format=format),
-        'testnode': reverse('testnode-list', request=request, format=format),
+        'speedtestclient': reverse('speedtestclient-list', request=request, format=format),
     })
 
 class SpeedTestList(generics.ListCreateAPIView):
@@ -28,15 +28,15 @@ class SpeedTestDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = SpeedTest.objects.all()
     serializer_class = SpeedTestSerializer
 
-class TestNodeList(generics.ListCreateAPIView):
+class SpeedTestClientList(generics.ListCreateAPIView):
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
-    queryset = TestNode.objects.all()
-    serializer_class = TestNodeSerializer
+    queryset = SpeedTestClient.objects.all()
+    serializer_class = SpeedTestClientSerializer
 
-class TestNodeDetail(generics.RetrieveUpdateDestroyAPIView):
+class SpeedTestClientDetail(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
-    queryset = TestNode.objects.all()
-    serializer_class = TestNodeSerializer
+    queryset = SpeedTestClient.objects.all()
+    serializer_class = SpeedTestClientSerializer
 
 class UserList(generics.ListAPIView):
     queryset = User.objects.all()
